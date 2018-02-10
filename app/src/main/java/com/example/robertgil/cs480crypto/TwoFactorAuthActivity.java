c8bccf638de8febf0457fbebe22347f480dd4664
Alec Strickland <alecjstrickland@gmail.com>
2018-02-10 15:27:29 -0800

    2FA UI

--- app/src/main/java/com/example/robertgil/cs480crypto/TwoFactorAuthActivity.java
+++ app/src/main/java/com/example/robertgil/cs480crypto/TwoFactorAuthActivity.java
@@ -2,31 +2,52 @@ package com.example.robertgil.cs480crypto;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.telephony.SmsManager;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.net.InetAddress;
 import java.net.Socket;
-import java.security.SecureRandom;
 
 public class TwoFactorAuthActivity extends AppCompatActivity {
 
     private final String TAG = "TwoFactorAuthActivity";
     private String code;
+    private EditText mCodeView;
+    private Button mContinueButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_two_factor_auth);
+        mCodeView = (EditText) findViewById(R.id.code);
+        mContinueButton = (Button) findViewById(R.id.continueButton);
         Bundle b = getIntent().getExtras();
         try {
             twoFactorAuth(b.get("email").toString());
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        mContinueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Wait for code to arrive in case socket is being slow
+                while (code == null) {
+                    Thread.yield();
+                }
+                if (codeIsCorrect()) {
+                    //TODO update UI for main screen or something i guess lmfaoooooooooo
+                }
+            }
+        });
+    }
+
+    private boolean codeIsCorrect() {
+        return mCodeView.getText().toString().equals(code);
     }
 
     private void twoFactorAuth(final String email) throws IOException {
@@ -42,13 +63,14 @@ public class TwoFactorAuthActivity extends AppCompatActivity {
 
     private class EmailAsker extends Thread {
 
+        /* Replace this field with whatever your public IP is my dude */
         private final String ipAddress = "134.71.136.130";
         private final int PORT = 25443;
         private String email;
         private ObjectInputStream ois;
         private String code;
 
-        EmailAsker(){}
+        private EmailAsker(){}
 
         EmailAsker(String email) {
             this.email = email;
