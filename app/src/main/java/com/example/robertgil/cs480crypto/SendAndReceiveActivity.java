3a82fcf889d8ff08afcc7ce3ed4d04c79a73ebf4
Al <agil6790@yahoo.com>
2018-03-01 06:02:39 -0800

    Wallet working properly, not finished

--- app/src/main/java/com/example/robertgil/cs480crypto/SendAndReceiveActivity.java
+++ app/src/main/java/com/example/robertgil/cs480crypto/SendAndReceiveActivity.java
@@ -1,22 +1,33 @@
 package com.example.robertgil.cs480crypto;
 
-import android.media.Image;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.json.JSONException;
+
+import java.io.IOException;
+
 
 public class SendAndReceiveActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private Spinner spinner;
+    TextView tv;
+    EditText amountEditText;
     private static final String[] paths = {"Dogecoin", "Bitcoin", "Litecoin"};
+    WalletModel model = new WalletModel();
+    WalletView view = new WalletView();
+    WalletController testRun = new WalletController(view, model);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,18 +35,50 @@ public class SendAndReceiveActivity extends AppCompatActivity implements Adapter
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_send_and_receive);
 
+        //Spinner SetUp
         spinner = findViewById(R.id.walletSpinnerID);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, paths);
-
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
+
+        tv = findViewById(R.id.adressToSendToText);
+
+        // NEED TO SET USERS API KEY BEFORE WALLET API WORKS
+        testRun.setAPIkey("e5ed-0847-8256-7f49"); // Need Api key for account access
+
+        //SET SERVICE FEE AT BOTTOM OF PAGE
+        //Edittext field of the amount, keeps a live view of the wallet service fee
+        amountEditText = findViewById(R.id.amountEditText);
+        amountEditText.setFocusableInTouchMode(true);
+        amountEditText.requestFocus();
+
+        amountEditText.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    new MyTask().execute(amountEditText.getText().toString());
+                    return true;
+                }
+                return false;
+            }
+        });
+
+
     }
+
+
+    public void accessAccountBalance(String APIKEY) throws IOException, JSONException {
+        String sb = "https://block.io/api/v2/get_balance/?api_key=";
+        sb += APIKEY;
+        model.setJsonResponse(model.getJSON(sb));
+    }
+
 
     /**
      * onItemSelected for the send and recieve spinner.
-    * Used to change the picture and the amount text when a particular option is chosen on the wallet spinner
-    * */
+     * Used to change the picture and the amount text when a particular option is chosen on the wallet spinner
+     */
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int pos, long id) {
         ImageView iv;
@@ -62,13 +105,45 @@ public class SendAndReceiveActivity extends AppCompatActivity implements Adapter
 
         }
     }
+
     /**
      * onNothingSelected for the send and recieve spinner.
      * Used when nothing on the spinner is selected
-     * */
+     */
     @Override
     public void onNothingSelected(AdapterView<?> adapterView) {
         Toast.makeText(this, "NOTHINGSELECTED", Toast.LENGTH_SHORT).show();
+
+    }
+
+
+    private class MyTask extends AsyncTask<String, String, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            //accessAccountBalance(strings[0]);
+            tv.setText(strings[0]);
+            return null;
+        }
+
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+
+            super.onPostExecute(s);
+        }
+
 
     }
 
---
+++ app/src/main/java/com/example/robertgil/cs480crypto/WalletView.java
@@ -0,0 +1,82 @@
+package com.example.robertgil.cs480crypto;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+All the visual calls for the console prototype, returns objects to parse from and can extract
+specific types of data depending on the call.
+* */
+
+public class WalletView {
+
+    public void getNetwork(JSONObject Response) throws JSONException {
+        System.out.println("Network : " + Response.getJSONObject("data").getString("network"));
+    }
+
+    public String getBalance(JSONObject Response) throws JSONException {
+        System.out.println("Balance : " + Response.getJSONObject("data").getString("available_balance"));
+        return Response.getJSONObject("data").getString("available_balance");
+    }
+
+    public void get_Amount_Sent(JSONObject Response) throws JSONException {
+        System.out.println("Amount Sent : " + Response.getJSONObject("data").getString("amount_sent"));
+
+    }
+
+    public void get_Network_Fee(JSONObject Response) throws JSONException {
+        System.out.println("Network Fee : " + Response.getJSONObject("data").getString("network_fee"));
+
+    }
+    public String get_estimated_network_fee(JSONObject Response) throws JSONException {
+        System.out.println("Estimated Network Fee : " + Response.getJSONObject("data").getString("estimated_network_fee"));
+        return Response.getJSONObject("data").getString("estimated_network_fee");
+
+    }
+
+
+    public void getStatus(JSONObject Response) throws JSONException {
+        System.out.println("Status : " + Response.getString("status"));
+    }
+
+    public void printAccountDetails(JSONObject Response) throws JSONException {
+        System.out.println("Status : " + Response.getString("status"));
+        System.out.println("Network : " + Response.getJSONObject("data").getString("network"));
+        System.out.println("Balance : " + Response.getJSONObject("data").getString("available_balance"));
+    }
+
+    public void obtain_Key_String() {
+        System.out.println("Please Enter API Key");
+    }
+
+    public void obtain_Secret_Key() {
+        System.out.println("Please Enter your Secret_Pin to withdraw funds");
+    }
+
+    public void obtain_Recipient_Address() {
+        System.out.println("Where would you like to send to? (Input address on network)");
+    }
+
+    public void obtain_Amount_To_Send() {
+        System.out.println("How much would you like to send?");
+    }
+
+    public void IncorrectOption() {
+        System.out.println("Incorrect option, please try again.");
+    }
+
+    public void pickOptionString() {
+        System.out.println("" +
+                "\n1. Check Account Balance." +
+                "\n2. Check Account Type" +
+                "\n3. Send Currency to an Address" +
+                "\n4. Print Account Details" +
+                "\n5. Exit" +
+                "\n6. Display Options"+
+                "\n7. Estimate Network Fee"
+        );
+
+    }
+
+
+
+}
