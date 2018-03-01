a282d9e5d3d1d0467605c97d4f70cee77a4feacf
hitTheRoad17 <yehanbo1216@gmail.com>
2018-03-01 14:33:15 -0800

    Analytics and Converter activity

---
+++ app/src/main/java/com/example/robertgil/cs480crypto/CardItems.java
@@ -0,0 +1,26 @@
+package com.example.robertgil.cs480crypto;
+
+public class CardItems {
+
+    private String currency;
+    private double btc_value, eth_value;
+
+    public CardItems(String currency, double btc_value, double eth_value) {
+        this.currency = currency;
+        this.btc_value = btc_value;
+        this.eth_value = eth_value;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public double getBtc_value() {
+        return btc_value;
+    }
+
+    public double getEth_value() {
+        return eth_value;
+    }
+
+}
