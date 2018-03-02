2ea075c8d5e77fa44da3dd99aca425141a0bf190
Al <agil6790@yahoo.com>
2018-03-02 03:21:56 -0800

    Wallet working properly, not finished

--- app/src/main/java/com/example/robertgil/cs480crypto/SendAndReceiveActivity.java
+++ app/src/main/java/com/example/robertgil/cs480crypto/SendAndReceiveActivity.java
@@ -1,13 +1,19 @@
 package com.example.robertgil.cs480crypto;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.Window;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
@@ -22,8 +28,9 @@ import java.io.IOException;
 public class SendAndReceiveActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private Spinner spinner;
-    TextView tv;
-    EditText amountEditText;
+    TextView serviceFeeID,balanceID;
+    EditText addressToSendToID,amountEditText;
+    Button sendButton;
     private static final String[] paths = {"Dogecoin", "Bitcoin", "Litecoin"};
     WalletModel model = new WalletModel();
     WalletView view = new WalletView();
@@ -35,35 +42,79 @@ public class SendAndReceiveActivity extends AppCompatActivity implements Adapter
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_send_and_receive);
 
-        //Spinner SetUp
+        //**********Spinner Set Up***************
         spinner = findViewById(R.id.walletSpinnerID);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, paths);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(this);
+        //****************************************
 
-        tv = findViewById(R.id.adressToSendToText);
+        //*********Button Initialization***********
+        Button sendBtn= findViewById(R.id.sendButtonID);
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                confirmDialog();
+            }
+        });
+        //******************************************
+
 
         // NEED TO SET USERS API KEY BEFORE WALLET API WORKS
         testRun.setAPIkey("e5ed-0847-8256-7f49"); // Need Api key for account access
 
-        //SET SERVICE FEE AT BOTTOM OF PAGE
-        //Edittext field of the amount, keeps a live view of the wallet service fee
-        amountEditText = findViewById(R.id.amountEditText);
-        amountEditText.setFocusableInTouchMode(true);
-        amountEditText.requestFocus();
 
-        amountEditText.setOnKeyListener(new View.OnKeyListener() {
+
+        serviceFeeID = findViewById(R.id.serviceFeeAmountID);
+        balanceID = findViewById(R.id.BalanceAmountTextView);
+
+
+
+
+    /**
+    * Receives input from the ADDRESS EditText Field
+    * Used to get input from user about which adress to send the coin too
+    * as soon as the accept button or enter is pressed its saved into the instance
+    * */
+        addressToSendToID = findViewById(R.id.adressToSendToText);
+        addressToSendToID.setOnKeyListener(new View.OnKeyListener() {
             public boolean onKey(View v, int keyCode, KeyEvent event) {
-                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
-                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
-                    new MyTask().execute(amountEditText.getText().toString());
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    Toast.makeText(SendAndReceiveActivity.this, "Address Received", Toast.LENGTH_SHORT).show();
+                    testRun.setRecipent_Adress(String.valueOf(addressToSendToID.getText()));
                     return true;
                 }
                 return false;
             }
         });
 
+    /**
+     * Receives input from the AMOUNT EditText Field
+     * Used to get input from user about the amount of coin to send.
+     * as soon as the accept button or enter is pressed its saved into the instance
+     * */
+        amountEditText = findViewById(R.id.amountEditText);
+        amountEditText.setInputType(InputType.TYPE_CLASS_NUMBER); // Only allow numbers in amount field
+        amountEditText.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    Toast.makeText(SendAndReceiveActivity.this, "Amount Received", Toast.LENGTH_SHORT).show();
+                    String test = "";
+                    test = String.valueOf(amountEditText.getText());
+                    double number = Double.valueOf(test);
+                    testRun.setAmount(number);
+
+
+                    return true;
+                }
+                return false;
+            }
+        });
+
+
+
+        new accessAccountBalance().execute(testRun.getApi_Key());
 
     }
 
@@ -102,7 +153,6 @@ public class SendAndReceiveActivity extends AppCompatActivity implements Adapter
                 iv.setImageResource(R.drawable.litecoin2);
                 tv.setText("LTC");
                 break;
-
         }
     }
 
@@ -116,36 +166,71 @@ public class SendAndReceiveActivity extends AppCompatActivity implements Adapter
 
     }
 
-
-    private class MyTask extends AsyncTask<String, String, String> {
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-        }
+    private class accessAccountBalance extends AsyncTask<String, String, String> {
 
         @Override
         protected String doInBackground(String... strings) {
-            //accessAccountBalance(strings[0]);
-            tv.setText(strings[0]);
+            try {
+                accessAccountBalance(strings[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
             return null;
         }
-
-
         @Override
-        protected void onProgressUpdate(String... values) {
-            super.onProgressUpdate(values);
-
+        protected void onPostExecute(String result) {
+            try {
+                balanceID.setText(view.getBalance(model.getJsonResponse()));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
-
-        @Override
-        protected void onPostExecute(String s) {
-
-            super.onPostExecute(s);
-        }
-
-
     }
 
 
+
+    /**
+     * Confirmation Diaglog box to confirm the sending of the cryptocurrency
+     * This prevents accidental sends on the app.
+     * Provides two options on the alert dialog, yes/no and runs the code accordingly
+     * */
+    private void confirmDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(SendAndReceiveActivity.this);
+
+        builder
+                .setMessage("Are you sure?")
+                .setPositiveButton("Yes, im sure.",  new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        Toast.makeText(SendAndReceiveActivity.this, "Yes", Toast.LENGTH_SHORT).show();
+                        //new sendConfirmationOnButtonPress().execute();
+                    }
+                })
+                .setNegativeButton("Nope nope nope", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog,int id) {
+                        Toast.makeText(SendAndReceiveActivity.this, "No", Toast.LENGTH_SHORT).show();
+                        dialog.cancel();
+                    }
+                })
+                .show();
+    }
+
+    private class sendConfirmationOnButtonPress extends AsyncTask<String, String, String> {
+        @Override
+        protected String doInBackground(String... strings) {
+            return null;
+        }
+        @Override
+        protected void onPostExecute(String result) {
+        }
+    }
+
 }
+
+
+
+
+
