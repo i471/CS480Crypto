8c9ece288051f9f0da7b6c8c4f86091b4e7ce665
Scrat <robertdgil@Hotmail.com>
2018-03-06 14:09:43 -0800

    Added Main Menu and options menu which includes About, Settings, Send, Markets
    Transition from login to main menu does not work

--- app/src/main/java/com/example/robertgil/cs480crypto/LoginActivity.java
+++ app/src/main/java/com/example/robertgil/cs480crypto/LoginActivity.java
@@ -453,7 +453,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
-                finish();
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                //intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
+                LoginActivity.this.finish();
+                //finish();
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
